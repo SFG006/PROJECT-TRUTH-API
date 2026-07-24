44cfefb462732f69bd7f1566d8e3e6d77e39b119
sfg <shivanshg005@gmail.com>
2026-07-24 14:56:05 +0530

    changes no of articles down to 400 to process

--- tactic_engine.py
+++ tactic_engine.py
@@ -77,7 +77,7 @@ def get_tactic_llm(text):
     try:
         chat_completion = client.chat.completions.create(
             messages=[{"role": "user", "content": prompt}],
-            model="meta-llama/llama-prompt-guard-2-22m",
+            model="openai/gpt-oss-20b",
             temperature=0,
             response_format={"type": "json_object"}
         )
